9eb3c28370c924d1840a9a90efa31fc02d7b56a6
Edson Zangiacomi Martinez <78192529+edsonzmartinez@users.noreply.github.com>
2024-10-29 00:45:08 -0300

    regressao_logistica_ML.r

--- regressao_logistica_ML.r
+++ regressao_logistica_ML.r
@@ -4,7 +4,7 @@
  urlfile <- "https://raw.githubusercontent.com/edsonzmartinez/cursoR/main/lowbwt.csv"
  dados <- read.csv2(urlfile)
  head(dados)
- library(carot)
+ library(caret)
  library(pROC)
  # Visitas ao médico
  table(dados$FTV)
@@ -28,6 +28,8 @@
  spec <- c(train = .75, test = .25)
  g    <- sample(cut(seq(nrow(dados)),nrow(dados)*cumsum(c(0,spec)),labels = names(spec)))
  res  <- split(dados, g)
+ dim(res$train) # matriz de treino
+ dim(res$test)  # matriz de teste
  lmodelt <- glm(modelo,data=res$train,family="binomial")
  summary(lmodelt)
  pred <- predict(lmodelt,newdata=res$test)
@@ -38,18 +40,30 @@
  true_neg  <- (true_y==0) & (pred_y==0)
  false_pos <- (true_y==0) & (pred_y==1)
  false_neg <- (true_y==1) & (pred_y==0)
- conf_mat <- matrix(c(sum(true_pos),sum(false_pos),sum(false_neg),sum(true_neg)),2,2)
- colnames(conf_mat) <- c("Yhat = 1","Yhat = 0")
- rownames(conf_mat) <- c("Y = 1","Y = 0")
+ conf_mat <- matrix(c(sum(true_pos),sum(false_neg),sum(false_pos),sum(true_neg)),2,2)
+ colnames(conf_mat) <- c("Y = 1","Y = 0")
+ rownames(conf_mat) <- c("Yhat = 1","Yhat = 0")
  conf_mat
+ # Sensibilidade e especificidade
+ sens  <- sum(true_pos)/(sum(true_pos)+sum(false_neg))
+ espec <- sum(true_neg)/(sum(true_neg)+sum(false_pos))
+ sens
+ espec
  # Precision, recall, F1 score
  precision <- diag(conf_mat) / rowSums(conf_mat) 
  recall <- diag(conf_mat) / colSums(conf_mat) 
  F1 <- 2 * (precision * recall) / (precision + recall)
+ # Precision, recall, F1 score usando o pacote caret
+ truth <- factor(ifelse(true_y==1,"A","B"))
+ prev <- factor(ifelse(pred_y==1,"A","B"))
+ caret::precision(prev,truth)
+ caret::recall(prev,truth)
+ confusion_matrix <- caret::confusionMatrix(table(prev, truth),mode="everything")
+ confusion_matrix
  # ROC curve
  roccurve <- roc(true_y,pred)
  plot(roccurve)
- cbind(roccurve$thresholds,roccurve$sensitivities,roccurve$specificities
+ cbind(roccurve$thresholds,roccurve$sensitivities,roccurve$specificities)
  ###
  ### Cross-validation K-fold
  ###
@@ -60,19 +74,26 @@
  # view summary of k-fold CV               
  print(modelK)
  modelK$resample
+ confusionMatrix(modelK)
  ###
  # Leave one out cross validation (LOOCV)
  ###
  # defining training control
  # as Leave One Out Cross Validation
  ctrl <- trainControl(method = "LOOCV")
- # training the model by assigning sales column
+ # training the model by assigning LOW column
  # as target variable and rest other column
  # as independent variable
  modelLOOCV <- train(modelo,data=dados,family="binomial", method = "glm", trControl = ctrl)
  # printing model performance metrics
  # along with other details
  print(modelLOOCV)
- modelLOOCV$resample
+ modelLOOCV$finalModel
  
+ ### Modelo resumido
+ modelo2 <- factor(LOW) ~ LWT + factor(RACE) + factor(SMOKE) + factor(PTL2) + factor(HT)
+ lmodel2 <- glm(modelo2,data=dados,family="binomial")
+ summary(lmodel2)
+ modelLOOCV <- train(modelo2,data=dados,family="binomial", method = "glm", trControl = ctrl)
+ print(modelLOOCV)
 
